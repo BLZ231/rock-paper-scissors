256760913e386291cf14b002fcc7eaf47f522f5c
BLZ231 <eriklarson231@gmail.com>
2025-03-28 17:38:00 -0400

    Working on pseudocode Establishing basic rules I sort of know what I'm doing but also sort of don't I suppose that's the purpose of practice, to get better

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log("Hello World")
+
+console.log("Testing One Two")
+
+/*
+New function getComputerChoice
+Have this function return 1 of 3 string values
+rock, paper, or scissors
+Have rock equal 1
+Have paper equal 2
+Have scissors equal 3
+Have the function return 1, 2, or 3, no other numbers
+
+New function getHumanChoice
+Use prompt to get the user's input
+Make input case insensitive
+Rock
+Paper
+Scissors
+
+Variables
+humanScore and computerScore
+Have them both start at 0
+
+function playRound
+takes humanChoice and computerChoice into account
+If hC is Rock and cC is Rock, return string A
+If hC is Rock and cC is Paper, return string B
+If hC is Rock and cC is Scissors, return string C
+If hC is Paper and cC is Rock, return string c
+If hC is Paper and cC is Paper, return string A
+If hC is Paper and cC is Scissors, return string B
+If hC is Scissors and cC is Rock, return string B
+If hC is Scissors and cC is Paper, return string C
+If hC is Scissors and cC is Scissors, return string A
+
+
+String A
+Draw.
+Neither score changes
+
+String B
+You lose.
+computerScore goes up by 1
+
+String C
+You win.
+humanScore goes up by 1
+
+function playGame
+Summons playRound function, I guess
+if computerScore equals 5, return string D
+if humanScore equals 5, return string E
+
+String D
+Sorry, you lost.
+
+String E
+Congratulations, you won.
+
+*/
+
+
+
+
+
+
+
+
+
+
+
